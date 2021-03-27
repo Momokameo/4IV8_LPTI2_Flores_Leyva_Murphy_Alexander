604fab54914f1e5bf663bdf92b691c4261934e02
Momokameo <72054645+Momokameo@users.noreply.github.com>
2021-03-26 19:01:19 -0600

    .

---
+++ js/main.js
@@ -0,0 +1,7 @@
+﻿ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards',{ delay: 600});
+ScrollReveal().reveal('.footer',{ delay: 600});
+ScrollReveal().reveal('.gods-cards',{ delay: 600});
+ScrollReveal().reveal('.recomend',{ delay: 600});
+ScrollReveal().reveal('.footer-footer',{ delay: 500});
+
